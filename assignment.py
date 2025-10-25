7766ebeef441fda271915a10f36606554a77e064
djfairbanks-utep <dallinfair96@gmail.com>
2025-10-25 08:56:16 -0600

    flake8 adjustments

--- assignment.py
+++ assignment.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import statsmodels.api as sm
 import matplotlib.pyplot as plt
-#If any of this libraries is missing from your computer. Please install them using pip.
+# If any of this libraries is missing from your computer,
+# please install them using pip.
 
 filename = 'Flight_Delays_2018.csv'
 
-#ARR_DELAY is the column name that should be used as dependent variable (Y).
+# ARR_DELAY is the column name that should be used as dependent variable (Y).
